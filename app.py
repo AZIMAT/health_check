f11342f1ae52a528d736c9cf4a8aeaa43b00a634
AZIMAT <atefiazim1997@gmail.com>
2022-10-03 23:03:04 +0330

    get online users

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import http.client as httplib
+import subprocess
+import json
 
 app = Flask(__name__)
 
@@ -9,12 +11,23 @@ health_status = True
 @app.route("/health")
 def health():
     conn = httplib.HTTPSConnection("radiojavan.com", timeout=5)
+    bashCommand = "yes q | docker exec -i any-pass occtl -j show events >> userx.json | head -n-3 userx.json > user.json"
+    output = subprocess.check_output(["bash", "-c", bashCommand])
+
+    # read file
+    with open("user.json", "r") as myfile:
+        data = myfile.read()
+
+    # parse file
+    obj = json.loads(data)
+    print(len(obj))
     try:
+
         conn.request("HEAD", "/")
-        resp = jsonify(health=1)
+        resp = jsonify(health=1, online=len(obj))
         resp.status_code = 200
     except Exception:
-        resp = jsonify(health=0)
+        resp = jsonify(health=0, online=len(obj))
         resp.status_code = 500
 
     return resp
